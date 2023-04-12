e2cbaf28c4528b6d97486d9f370a8c08d143f5f5
Oriowo Joshua <Joshuatobi8@gmail.com>
2023-04-11 19:39:02 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,14 @@
  *
  * Return: value of the bit
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int m, unsigned int index)
 {
 	int bit_val;
 
 	if (index > 63)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	bit_val = (m >> index) & 1;
 
 	return (bit_val);
 }
